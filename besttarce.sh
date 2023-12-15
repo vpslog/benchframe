5d57348349d96f864f754609d0d0e46a70f2c2bb
grayit0 <132592119+grayit0@users.noreply.github.com>
2023-12-15 15:44:43 +0800

    Create besttarce.sh

---
+++ besttarce.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# 执行安装命令
+bash <(curl -Ls https://raw.githubusercontent.com/sjlleo/nexttrace/main/nt_install.sh)
+
+# 等待一段时间，确保安装完成
+
+# 运行程序 nexttrace，并模拟输入 1，回车，6，回车
+echo -e "1\n6\n" | nexttrace --fast-trace
