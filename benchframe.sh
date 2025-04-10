342307a65d392d8a954444f931f85df9474b9821
grayit0 <132592119+grayit0@users.noreply.github.com>
2025-04-10 21:49:53 +0800

    Update benchframe.sh

--- benchframe.sh
+++ benchframe.sh
@@ -106,7 +106,7 @@ run_script "FIO 测试" "https://raw.githubusercontent.com/vpslog/benchframe/mai
 # run_script "三网测速" "https://raw.githubusercontent.com/i-abc/Speedtest/main/speedtest.sh" '2\n\n'
 run_script "三网测速" "https://bench.im/hyperspeed" '\n\n'
 run_script "教育网测速" "https://bench.im/hyperspeed" '6\n\n'
-run_script "线路测试" "https://raw.githubusercontent.com/zhanghanyun/backtrace/main/install.sh"
+# run_script "线路测试" "https://raw.githubusercontent.com/zhanghanyun/backtrace/main/install.sh"
 # run_script "流媒体解锁测试" "https://raw.githubusercontent.com/lmc999/RegionRestrictionCheck/main/check.sh" "1\n"
 run_script "流媒体解锁测试" "https://media.ispvps.com" "1\n"
 run_script "回程路由测试" "https://raw.githubusercontent.com/vpslog/benchframe/main/besttarce.sh"
